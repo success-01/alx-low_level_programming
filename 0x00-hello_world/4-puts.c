3bd8429b9274c3d747626af75f5664fdd67015d1
success-01 <nompumeleloxaba972@gmail.com>
2023-07-14 13:28:37 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("with proper grammar, but the outcome is a piece of art\n");
+        return (0);
+}
