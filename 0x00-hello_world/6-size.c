ce21b3b026603e8c996bed288af97a0a55d8594b
success-01 <nompumeleloxaba972@gmail.com>
2023-07-15 00:42:47 -0700

    printf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+	float f;
+	long int;
+	long long int;
+
+	printf("The size of int is:%lu.\n
+    return (0);
+}
