9cb2ed0ccefbed3b4d9adceaee06fe8a58694fec
success-01 <nompumeleloxaba972@gmail.com>
2023-08-15 06:01:43 -0700

    fresh start

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -27,3 +27,16 @@ int main(int a, int b, int c)
 
 	return (largest);
 }
+
+int main(void) 
+{
+	int a, b, c;
+
+	printf("Enter three integers: ");
+	scanf("%d %d %d", &a, &b, &c);
+
+	int result = largest_number(a, b, c);
+	printf("The largest number is: %d\n", result);
+
+	return 0;
+}
--- 0x0A-argc_argv/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-int _putchar(char c);
-int _atoi(char *s);
-
-#endif
