5ac04da337142b0778f7dc38b3d78b5d67f55764
success-01 <nompumeleloxaba972@gmail.com>
2023-09-19 15:14:35 -0700

    header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's info
+ * @name:the first memmber
+ * @age:the second member
+ * @owner:the third member
+ *
+ * Description:lengthy desciption
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - structdogs typedef
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
