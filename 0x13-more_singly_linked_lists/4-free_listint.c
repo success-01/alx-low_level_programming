8e917dbcf0797c0a7f0c8de592b28f87c4e025c9
success-01 <nompumeleloxaba972@gmail.com>
2023-09-26 04:12:53 -0700

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 /**
  * free_listint - func that frees a listint_t list
- * @head:pointer to head of the listint_t list 
+ * @head:pointer to head of the listint_t list
  */
 void free_listint(listint_t *head)
 {
