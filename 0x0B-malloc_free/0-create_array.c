fdf21c73a0cc06598205329fe4c1e2a7e9bc8d44
success-01 <nompumeleloxaba972@gmail.com>
2023-10-31 13:43:48 -0700

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,23 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - creates array of chars, initialize with specific bar
- * @c: char to initialize
- * @size: no. of bytes to be allocated
- * Return: pointer to array or null if fail
+ * *create_array - creates arrays of chars and initializes them
+ * @c:char
+ * @size:size of array
+ * Description:create array of size and assign to char
+ * Return: pointer to array or NULL if it fails
  */
 char *create_array(unsigned int size, char c)
 {
 	char *str;
-	unsigned int k;
+	unsigned int m;
 
 	str = malloc(sizeof(char) * size);
-
 	if (size == 0 || str == NULL)
 		return (NULL);
 
-	for (k = 0; k < size; k++)
-		str[k] = c;
-	return (str);
+	for (m = 0; m < size; m++)
+		str[m] = c;
 
+	return (str);
 }
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,1 +1,1 @@
-my readme
+0x0B-malloc_free
