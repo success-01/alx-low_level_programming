82b0d55142ff22a89e6f97cd557f2e82357a7f77
success-01 <nompumeleloxaba972@gmail.com>
2024-03-21 04:32:05 -0700

    variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,18 +2,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * print_all - prints anything and everything
+ * print_all - prints any given argument
  * @format:argument specifier
  * Return:any argument given based on format specifier
  */
 void print_all(const char * const format, ...)
 {
-	int m, check_stat;
+	int m, check_stat; /*declared variable and datatype*/
 	char *str;
 
 	va_list all;
 
-	va_start(all, format);
+	va_start(all, format); /*variable argument*/
 
 	m = 0;
 	while (format != NULL && format[m] != '\0')
@@ -26,7 +26,7 @@ void print_all(const char * const format, ...)
 					break;
 				case 'f':
 					printf("%f", va_arg(all, double));
-					check_stat = 0;
+					check_stat = 0; /*check if condition is met*/
 					break;
 				case 'c':
 					printf("%c", va_arg(all, int));
@@ -42,10 +42,10 @@ void print_all(const char * const format, ...)
 					check_stat = 1;
 					break;
 			}
-			if (format[m + 1] != '\0' && check_stat == 0)
+			if (format[m + 1] != '\0' && check_stat == 0) /*if not NULL*/
 				printf(", ");
-			m++;
+			m++; /*update step of iter variable*/
 		}
-		printf("\n");
-		va_end(all);
+		printf("\n");/*print new line*/
+		va_end(all);/*end traversal*/
 }
