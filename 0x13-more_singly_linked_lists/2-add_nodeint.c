4f6f6bca7111d561f11c0743d4585beaf944058b
success-01 <nompumeleloxaba972@gmail.com>
2024-03-21 06:22:35 -0700

    node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,7 +10,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new_node;
 
 	if (head == NULL)
-		return (0);
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
@@ -22,5 +22,5 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new_node->n = n;
 	*head = new_node;
 
-	return (0);
+	return (*head);
 }
