4be6f08483878d061afa817582927570ff2616ce
success-01 <nompumeleloxaba972@gmail.com>
2024-03-21 06:17:05 -0700

    node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_nodeint - add note to beginning if list
+ * @head:pointer to head pointer where the head points to first node
+ * @n:new int. to be created
+ * Return:the head
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	if (head == NULL)
+		return (0);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+	new_node->n = n;
+	*head = new_node;
+
+	return (0);
+}
